c0d075076f533d43e82cdb3785659fac1d23ddc0
Ubaid Mehmood <52918940+ubaidwarraich@users.noreply.github.com>
2020-07-28 16:19:37 +0500

    added js file

---
+++ app.js
@@ -0,0 +1,10 @@
+const add_btn=document.getElementById('add-btn');
+const input_val=document.getElementById('value');
+const list=document.getElementById('list-menu-items');
+add_btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let li=document.createElement('li');
+    li.innerText=input_val.value;
+    list.appendChild(li);
+    console.log(li);
+})
---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        form{
+            display:flex;
+            flex-direction:row;
+        }
+    </style>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="" id="value">
+        <input type="submit" id="add-btn"value="Add">
+    </form>
+    <ul id="list-menu-items">
+    
+    </ul>
+    <script src="app.js"></script>
+</body>
+</html>
